17460a47067a6a207a090d28807d7473927cc017
Alex Ikezili <alex.ikezili@icloud.com>
2025-06-24 08:51:12 -0300

    :sparkles: Criação das Páginas de Cadastro e Login

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -13,9 +13,15 @@ function Footer() {
                         </p>
                     <p className='text-lg'>Acesse nossas redes sociais</p>
                     <div className='flex gap-2'>
-                        <LinkedinLogo size={48} weight='bold' />
-                        <InstagramLogo size={48} weight='bold' />
-                        <FacebookLogo size={48} weight='bold' />
+                        <a href='' target='_blank'>
+                            <LinkedinLogo size={48} weight='bold' />
+                        </a>
+                        <a href='' target='_blank'>
+                            <InstagramLogo size={48} weight='bold' />
+                        </a>
+                        <a href='' target='_blank'>
+                            <FacebookLogo size={48} weight='bold' />
+                        </a>
                     </div>
                 </div>
             </div>
